8de110ea1a10266231d982997dd90bc68892d6c3
e1iguerron789 <eliguerron78@hotmail.com>
2023-05-03 14:58:14 -0500

    Empezamos haciendo el cuerpo inicial del codigo

---
+++ ejercicio3.c
@@ -0,0 +1,12 @@
+/*Construya un programa que calcule la suma de los términos de la serie: El usuario debe ingresar el valor de n. Tenga en cuenta que entre más grande sea n, el resultado debe acercarse a 1.*/
+
+#include <stdio.h>
+int main ()
+{
+    int num;
+    printf("    Bienvenido\n");
+    printf("Ingrese un numero:");
+    scanf("%d",&num);
+  
+  return 0 ;
+}
