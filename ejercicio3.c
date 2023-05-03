c2adcffefb006a042d6dcbf94193af174d649fa2
e1iguerron789 <eliguerron78@hotmail.com>
2023-05-03 15:32:09 -0500

    La idea sobre como hacer la potencia del denominador

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,12 +1,25 @@
 /*Construya un programa que calcule la suma de los términos de la serie: El usuario debe ingresar el valor de n. Tenga en cuenta que entre más grande sea n, el resultado debe acercarse a 1.*/
 
 #include <stdio.h>
+#include <math.h>
 int main ()
 {
-    int num;
+    int i,num,cont,den=2,expon;
+    
     printf("    Bienvenido\n");
-    printf("Ingrese un numero:");
+    printf("Ingrese el numero de veces que quiere repetir la serie:");
     scanf("%d",&num);
+
+    do{
+        for (i=1;i<=num;i++){
+            
+          den=pow(den,i);
+           printf("%d\n",den);  
+        }
+         cont++;
+    }while(cont<=num);
+
+    
   
   return 0 ;
 }
